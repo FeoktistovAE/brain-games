16ce7e3448cd06d17fda98b3333390f5e8246e7c
root <root@DESKTOP-6RAUT71.localdomain>
2022-04-28 11:30:50 +0300

    final fix 9*

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -8,5 +8,5 @@ DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no"'
 def get_expression_and_result():
     number = randint(1, 100)
     if number % 2 == 0:
-        return  number, 'yes'
+        return number, 'yes'
     return number, 'no'
